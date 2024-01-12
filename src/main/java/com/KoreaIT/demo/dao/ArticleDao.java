e3ff990de6d10505c81d2c5ee1be92e8901a3898
jangtjdgns <jangtjdgns0208@gmail.com>
2024-01-12 19:52:30 +0900

    MyBatis 설치, 세팅 및 DB 연동 테스트

--- src/main/java/com/KoreaIT/demo/dao/ArticleDao.java
+++ src/main/java/com/KoreaIT/demo/dao/ArticleDao.java
@@ -1,64 +1,28 @@
 package com.KoreaIT.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.KoreaIT.demo.vo.Article;
 
-@Component // DB 연결을 하지 않았을 경우 DAO에서 @Componenet 어노테이션 사용 (관리만 하는 상태)
-public class ArticleDao {
-	private int lastArticleId;
-	List<Article> articles;
+@Mapper
+public interface ArticleDao {
 
-	public ArticleDao() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		makeTestData();
-	}
+	public Article writeArticle(String title, String body);
+	
+	@Select(""" 
+			SELECT *
+				FROM article
+				WHERE id = #{id}
+			""")
+	
+	public Article getArticleById(int id);
 
-	// 테스트 게시물 생성 메서드
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			writeArticle("제목" + i, "내용" + i);
-		}
-	}
+	public void deleteArticle(Article article);
 
-	// 게시물 추가 메서드
-	public Article writeArticle(String title, String body) {
-		lastArticleId++;
+	public void modifyArticle(Article article, String title, String body);
 
-		Article article = new Article(lastArticleId, title, body);
-
-		articles.add(article);
-
-		return article;
-	}
-
-	// id로 게시물 가져오는 메서드
-	public Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-
-		return null;
-	}
-
-	// 삭제 메서드
-	public void deleteArticle(Article article) {
-		articles.remove(article);
-	}
-
-	// 수정 메서드
-	public void modifyArticle(Article article, String title, String body) {
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	public List<Article> showList() {
-		return articles;
-	}
+	public List<Article> showList();
 }
