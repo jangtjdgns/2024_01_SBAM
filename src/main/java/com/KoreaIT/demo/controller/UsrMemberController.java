21420a11101fd6bf44d0f37edba7b9fbb6edab2d
jangtjdgns <jangtjdgns0208@gmail.com>
2024-01-16 19:42:33 +0900

    ResultData

--- src/main/java/com/KoreaIT/demo/controller/UsrMemberController.java
+++ src/main/java/com/KoreaIT/demo/controller/UsrMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.KoreaIT.demo.service.MemberService;
 import com.KoreaIT.demo.util.Util;
 import com.KoreaIT.demo.vo.Member;
+import com.KoreaIT.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -19,37 +20,36 @@ public class UsrMemberController {
 	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 		
 		if (Util.empty(loginId)) {
-			return "아이디를 입력해주세요.";
+			return ResultData.from("F-1", "아이디를 입력해주세요.");
 		}
 		
 		if (Util.empty(loginPw)) {
-			return "비밀번호를 입력해주세요.";
+			return ResultData.from("F-2", "비밀번호를 입력해주세요.");
 		}
 		
 		if (Util.empty(name)) {
-			return "이름을 입력해주세요.";
+			return ResultData.from("F-3", "이름을 입력해주세요.");
 		}
 		
 		if (Util.empty(nickname)) {
-			return "닉네임을 입력해주세요.";
+			return ResultData.from("F-4", "닉네임을 입력해주세요.");
 		}
 		
 		if (Util.empty(cellphoneNum)) {
-			return "전화번호를 입력해주세요.";
+			return ResultData.from("F-5", "전화번호를 입력해주세요.");
 		}
 		
 		if (Util.empty(email)) {
-			return "이메일을 입력해주세요.";
+			return ResultData.from("F-6", "이메일을 입력해주세요.");
 		}
 		
 		Member member = memberService.getMemberByLoginId(loginId);
 
 		if (member != null) {
-//			return String.format("%s은(는) 이미 사용중인 아이디입니다.", loginId) ;
-			return Util.f("%s은(는) 이미 사용중인 아이디입니다.", loginId);
+			return ResultData.from("F-A", Util.f("%s은(는) 이미 사용중인 아이디입니다.", loginId));
 		}
 		
 		memberService.joinMember(loginId, loginPw, name, nickname, cellphoneNum, email);
@@ -58,6 +58,6 @@ public class UsrMemberController {
 		
 		member = memberService.getMemberById(id);
 
-		return member;
+		return ResultData.from("S-1", "회원가입 성공", member);
 	}
 }
---
+++ src/main/java/com/KoreaIT/demo/vo/ResultData.java
@@ -0,0 +1,37 @@
+package com.KoreaIT.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultData {
+	private String resultCode;
+	private String msg;
+	private Object data;
+	
+	public static ResultData from(String resultCode, String msg) {
+		return from(resultCode, msg, null);
+	}
+	
+	public static ResultData from(String resultCode, String msg, Object data) {
+		return new ResultData(resultCode, msg, data);
+	}
+	
+	/* 
+	 * 이 메서드는 롬복과 관계없이 변수가 생성되는데,
+	 * 이는 boolean 타입의 메서드이기 때문에 가능함
+	 * boolean 타입의 메서드명이 is로 시작될때 생성됨
+	 */
+	public boolean isSuccess() {
+		// 결과코드가 S-로 시작되면 true 
+		return this.resultCode.startsWith("S-");
+	}
+	
+	public boolean isFail() {
+		// 결과코드가 S-가 아니라면 true 
+		return !isSuccess();
+	}
+}
