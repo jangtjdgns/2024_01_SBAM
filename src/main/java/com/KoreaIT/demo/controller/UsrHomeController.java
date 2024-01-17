545b78047524a956a3fa482e79a6d1e271bfac7d
jangtjdgns <jangtjdgns0208@gmail.com>
2024-01-17 20:59:20 +0900

    jsp connect, main.jsp

--- src/main/java/com/KoreaIT/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/demo/controller/UsrHomeController.java
@@ -2,16 +2,19 @@ package com.KoreaIT.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	
-	@RequestMapping("/usr/home/showMain")
-	@ResponseBody
+	@RequestMapping("/usr/home/main")
 	public String showMain() {
 		
-		return "안녕하세요.";
+		return "usr/home/main";
 	}
 	
+	@RequestMapping("/")
+	public String showRoot() {
+		
+		return "redirect:/usr/home/main";
+	}
 }
